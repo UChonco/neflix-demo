bd9df7db46ec66a5d41f005af0addd7952082034
Unathi <uchonco21@gmail.com>
2023-12-28 14:34:43 +0200

    added javasrcipt

---
+++ index.js
@@ -0,0 +1,11 @@
+function dark(){
+    document.body.style.background="black";
+    document.body.style.color="white";
+
+}
+
+function light(){
+    document.body.style.background="white";
+    document.body.style.color="black";
+
+}
